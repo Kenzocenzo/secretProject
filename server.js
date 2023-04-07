73fd508b78f8596e7f13cccacbb87de824a4eefb
Kenzocenzo <aleksrogoz@gmail.com>
2023-04-07 20:18:46 +0200

    Nice things!

--- server.js
+++ server.js
@@ -7,9 +7,14 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.static("static"));
 const images = require("./static/json/images.json");
+const gallery = require("./static/json/gallery.json");
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname + "/static/html/index.html"));
+});
+
+app.get("/wishes", function (req, res) {
+    res.sendFile(path.join(__dirname + "/static/html/wishes.html"));
 });
 
 app.get("/random", function (req, res) {
@@ -17,12 +22,16 @@ app.get("/random", function (req, res) {
 });
 
 app.get("/randomImg", function (req, res) {
-    res.send(JSON.stringify(images[Math.floor(Math.random() * images.length)]));
+    let x = Math.floor(Math.random() * images.length);
+    res.send(JSON.stringify({ max: images.length, index: x, ...images[x] }));
 });
 
 app.get("/gallery", function (req, res) {
-    console.log(req.body);
-    res.send(JSON.stringify("123"));
+    res.sendFile(path.join(__dirname + "/static/html/gallery.html"));
+});
+
+app.get("/galleryImgs", function (req, res) {
+    res.send(JSON.stringify(gallery));
 });
 
 app.listen(PORT, function () {
